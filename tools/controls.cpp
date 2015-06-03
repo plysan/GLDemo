b7d0d1e32f162e0a1f2b9f0484c201e2e05d92b5
plysan <plysab@gmail.com>
2015-06-03 14:09:59 +0800

    Remove tabs: s/\t/    /g
    Signed-off-by: plysan <plysab@gmail.com>

--- tools/controls.cpp
+++ tools/controls.cpp
@@ -15,10 +15,10 @@ glm::mat4 ViewMatrix;
 glm::mat4 ProjectionMatrix;
 
 glm::mat4 getViewMatrix(){
-	return ViewMatrix;
+    return ViewMatrix;
 }
 glm::mat4 getProjectionMatrix(){
-	return ProjectionMatrix;
+    return ProjectionMatrix;
 }
 glm::vec3 up;
 glm::vec3 tangent(1.0f, 1.0f, 1.0f);
@@ -45,23 +45,23 @@ void computeMatricesFromInputs(){
 
     up = glm::normalize(vertex_offset);
 
-	// glfwGetTime is called only once, the first time this function is called
-	static double lastTime = glfwGetTime();
+    // glfwGetTime is called only once, the first time this function is called
+    static double lastTime = glfwGetTime();
 
-	// Compute time difference between current and last frame
-	double currentTime = glfwGetTime();
-	float deltaTime = (float)(currentTime - lastTime);
+    // Compute time difference between current and last frame
+    double currentTime = glfwGetTime();
+    float deltaTime = (float)(currentTime - lastTime);
 
-	// Get mouse viewPos
-	double xpos, ypos;
-	glfwGetCursorPos(window, &xpos, &ypos);
+    // Get mouse viewPos
+    double xpos, ypos;
+    glfwGetCursorPos(window, &xpos, &ypos);
 
-	// Reset mouse viewPos for next frame
-	glfwSetCursorPos(window, windowW/2, windowH/2);
+    // Reset mouse viewPos for next frame
+    glfwSetCursorPos(window, windowW/2, windowH/2);
 
-	// Compute new orientation
-	horizontalAngle += mouseSpeed * float( windowW/2 - xpos );
-	verticalAngle += mouseSpeed * float( windowH/2 - ypos );
+    // Compute new orientation
+    horizontalAngle += mouseSpeed * float( windowW/2 - xpos );
+    verticalAngle += mouseSpeed * float( windowH/2 - ypos );
 
     glm::vec3 direction = glm::cross(up, tangent);
     direction = glm::rotate(direction, horizontalAngle, up);
@@ -70,40 +70,40 @@ void computeMatricesFromInputs(){
     glm::vec3 right = glm::cross(direction, up);
 
     viewObj->position = viewPos;
-	// Move forward
-	if (glfwGetKey( window, GLFW_KEY_W ) == GLFW_PRESS){
-		viewObj->velocity += direction * deltaTime * speed;
-	}
-	// Move backward
-	if (glfwGetKey( window, GLFW_KEY_S ) == GLFW_PRESS){
-		viewObj->velocity -= direction * deltaTime * speed;
-	}
-	// Strafe right
-	if (glfwGetKey( window, GLFW_KEY_D ) == GLFW_PRESS){
-		viewObj->velocity += right * deltaTime * speed;
-	}
-	// Strafe left
-	if (glfwGetKey( window, GLFW_KEY_A ) == GLFW_PRESS){
-		viewObj->velocity -= right * deltaTime * speed;
-	}
+    // Move forward
+    if (glfwGetKey( window, GLFW_KEY_W ) == GLFW_PRESS){
+        viewObj->velocity += direction * deltaTime * speed;
+    }
+    // Move backward
+    if (glfwGetKey( window, GLFW_KEY_S ) == GLFW_PRESS){
+        viewObj->velocity -= direction * deltaTime * speed;
+    }
+    // Strafe right
+    if (glfwGetKey( window, GLFW_KEY_D ) == GLFW_PRESS){
+        viewObj->velocity += right * deltaTime * speed;
+    }
+    // Strafe left
+    if (glfwGetKey( window, GLFW_KEY_A ) == GLFW_PRESS){
+        viewObj->velocity -= right * deltaTime * speed;
+    }
     if (glfwGetKey( window, GLFW_KEY_SPACE ) == GLFW_PRESS){
-		viewObj->velocity += up * deltaTime * speed;
-	}
+        viewObj->velocity += up * deltaTime * speed;
+    }
     if (glfwGetKey( window, GLFW_KEY_LEFT_SHIFT ) == GLFW_PRESS){
-		viewObj->velocity -= up * deltaTime * speed;
-	}
+        viewObj->velocity -= up * deltaTime * speed;
+    }
     viewPos = viewObj->nextPos();
-	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	ProjectionMatrix = glm::perspective(initialFoV, (float)windowW / (float)windowH, 0.01f, 10000.0f);
-	// Camera matrix
+    // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+    ProjectionMatrix = glm::perspective(initialFoV, (float)windowW / (float)windowH, 0.01f, 10000.0f);
+    // Camera matrix
     direction *= 10000.0f;
     glm::vec3 posdir = viewPos+direction;
-	ViewMatrix       = glm::lookAt(
-								viewPos,           // Camera is here
-								viewPos + direction, // and looks here : at the same viewPos, plus "direction"
-								up                  // Head is up (set to 0,-1,0 to look upside-down)
-						   );
+    ViewMatrix       = glm::lookAt(
+                                viewPos,           // Camera is here
+                                viewPos + direction, // and looks here : at the same viewPos, plus "direction"
+                                up                  // Head is up (set to 0,-1,0 to look upside-down)
+                           );
 
-	// For the next frame, the "last time" will be "now"
-	lastTime = currentTime;
+    // For the next frame, the "last time" will be "now"
+    lastTime = currentTime;
 }
